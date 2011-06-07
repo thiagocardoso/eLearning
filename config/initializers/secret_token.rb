52f58df562bdd81ae95582e64a3882b0456a5282
Thiago Cardoso <thiagocardoso.s@gmail.com>
2011-06-07 00:39:51 -0300

    Projeto eLearning rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ELearning::Application.config.secret_token = '8b6e622399b803c209f5b3be23a4ee2390cb7c54fa40db4e4077b54d6bade1d963303b686a9d462ef5014b350d8cbf19c9ef1d5a08a8cbb1aeb9687675f1cd3c'
