8b992bbf7169e79d5549e86488ed4791667eeef7
Tadashi <tadashi@tadashi.(none)>
2011-11-26 18:02:03 -0200

    cadastro de questoes objetivas, relacione as colunas

--- app/models/questao.rb
+++ app/models/questao.rb
@@ -1,4 +1,7 @@
 class Questao < ActiveRecord::Base
   has_many :avaliacao_questaos
   has_many :avaliacaos, :through => :avaliacao_questaos
+  
+  belongs_to :questao_alternativas
+  has_many :questao_alternativas
 end
--- app/models/questao_alternativas.rb
+++ app/models/questao_alternativas.rb
@@ -1,4 +1,8 @@
 class QuestaoAlternativas < ActiveRecord::Base
   belongs_to :questao_alternativas
   has_many :questao_alternativas
+  
+  belongs_to :questao
+  
+  
 end
